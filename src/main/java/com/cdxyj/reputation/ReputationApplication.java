22a5d3d938c3b1383327ec98782e30c6efea7de0
jiangwww <1198824901@qq.com>
2019-09-18 20:14:42 +0800

    提交文文件

---
+++ src/main/java/com/cdxyj/reputation/ReputationApplication.java
@@ -0,0 +1,13 @@
+package com.cdxyj.reputation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReputationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReputationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdxyj/reputation/ReputationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cdxyj.reputation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReputationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
