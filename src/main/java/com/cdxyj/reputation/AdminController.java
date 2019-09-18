54bdbef9f3c3328a3459b14b37e04845d64b2963
jiangwww <1198824901@qq.com>
2019-09-18 21:45:53 +0800

    initialize

---
+++ src/main/java/com/cdxyj/reputation/AdminController.java
@@ -0,0 +1,13 @@
+package com.cdxyj.reputation;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Author jiangwei
+ * @Description
+ * @Date 2019/9/18
+ **/
+@Controller("/admin")
+public class AdminController {
+
+}
--- src/main/java/com/cdxyj/reputation/HelloWorldController.java
+++
@@ -1,9 +0,0 @@
-package com.cdxyj.reputation;
-
-/**
- * @Author jiangwei
- * @Description
- * @Date 2019/9/18
- **/
-public class HelloWorldController {
-}
